6b093a35a005d8b0831be959553f956d95e843f8
Luca Utzeri <lutzeri@libero.it>
2024-03-19 18:57:38 +0100

    test html/css to check functionality (WIP)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CardServiceService } from '../services/card-service.service';
+import { AllCards } from '../interfaces/card';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cards: AllCards[] = [];
+  currentPage = 1;
+  pageSize = 20;
+
+
+  constructor(private cardSrv: CardServiceService) { }
 
   ngOnInit(): void {
+    this.loadCards();
+  }
+
+  loadCards(): void {
+    this.cardSrv.getCards(this.currentPage, this.pageSize)
+      .subscribe(cards => { this.cards = cards });
+  }
+
+  nextPage(): void {
+    this.currentPage++;
+    this.loadCards();
+  }
+
+  previousPage(): void {
+    this.currentPage--;
+    this.loadCards();
+  }
+
+  setPage(page: number): void {
+    this.currentPage = page;
+    this.loadCards();
+  }
+
+  testSaveImage() {
+    this.cardSrv.saveCardImages(this.cards);
   }
 
 }
